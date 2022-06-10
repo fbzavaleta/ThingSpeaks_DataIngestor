4dc5280691613fb4fbf7bf8675a948c2260a575a
Julia <juliasbardelatti@gmail.com>
2022-06-09 22:45:41 -0300

    finalizando projeto

--- app/objects/Model.py
+++ app/objects/Model.py
@@ -1,7 +1,8 @@
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
 import numpy as np
+import pandas as pd 
 
 class Model:
     def __init__(self, df):
@@ -9,7 +10,22 @@ class Model:
         self.y = df['label']
     
     def criarModel(self):
-        X_train, X_test, y_train, y_test = train_test_split(self.X, self.y, test_size=0.3, stratify=self.y, random_state=42)
-        clf = RandomForestClassifier(max_depth=2, random_state=0)
-        clf.fit(X_train, y_train)
-        return clf
+        X_train, X_test, y_train, y_test = train_test_split(self.X, self.y, test_size=0.3, random_state=42)
+        lr = LinearRegression()
+        lr.fit(X_train, y_train)
+
+
+        intercept = lr.intercept_
+        coef = lr.coef_
+
+        y_pred = lr.predict(X_test)
+        df_test = pd.DataFrame({'atual': y_test, 'predicted': y_pred})
+
+        print('-------------------------------- model results ------------------------------------')
+        print('r2 score:', metrics.r2_score(y_test, y_pred))
+        print('mean absolute error:', metrics.mean_absolute_error(y_test, y_pred))
+        print('mean squared error:', metrics.mean_squared_error(y_test, y_pred))
+        print('root mean squared error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+
+
+        return lr, df_test
