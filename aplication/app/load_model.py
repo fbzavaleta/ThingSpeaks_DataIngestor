5a11962f6e9ea4303753889c593f9c2215280d9e
benjamin <benjamin.zavaleta@grieletlabs.com>
2023-03-08 21:50:41 -0300

    Revert "first"
    This reverts commit 0dc9d1444859650c5c0474ff60e65fd94e1c88fa.

--- aplication/app/load_model.py
+++
@@ -1,9 +0,0 @@
-import pickle
-from sklearn.linear_model import LinearRegression
-
-class LoadLoad():
-    def __init__(self, filename):      
-        with open(filename , 'rb') as f:
-            loaded_model = pickle.load(f)            
-        self.model = loaded_model
-    
